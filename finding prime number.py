c3380b6dcd7dedf12100370e124e0177dc2199a0
Fevzi Furkan Demirbuğa <103340757+fevzifurkandemirbuga@users.noreply.github.com>
2022-09-27 22:14:00 +0300

    Update finding prime number.py

--- finding prime number.py
+++ finding prime number.py
@@ -3,7 +3,8 @@ print(f"prime numbers from 0 to {a}")
 for i in range(2, a+1):
     control = 0
     for j in range(2, i):
-        if (i % j) == 0:
+        if i % j== 0:
             control = 1
     if control == 0:
         print(i)
+
